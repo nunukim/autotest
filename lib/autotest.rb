seattlerb/zentest

commit dc4ae0bc129de83bd72a627a36a54f586aec0a40

- 1.9 compat: don't use putc anymore. (fistfvck (hah!))
[git-p4: depot-paths = "//src/ZenTest/dev/": change = 5602]

lib/autotest.rb
@@ -243,7 +243,7 @@ class Autotest
       open("| #{cmd}", "r") do |f|
         until f.eof? do
           c = f.getc or break
-          putc c
+          print c
           line << c
           if c == ?\n then
             self.results << if RUBY_VERSION >= "1.9" then

nunukim/autotest

commit b85327a4a2274600923b346df2970917032fe7cc

fix bug introduced in dc4ae0bc129de83bd72a627a36a54f586aec0a40

bin/autotest
@@ -31,6 +31,8 @@ class Dir
 end
 
 #run the correct Autotest variant fitting to the local structure
+$LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../lib")
+
 require 'autotest'
 Autotest.options.merge!(options)
 target = Autotest
@@ -43,6 +45,7 @@ unless style.empty? then
   rescue LoadError
     abort "Autotest style #{mod} doesn't seem to exist. Aborting."
   end
+  puts "style: #{style.map {|s| s.capitalize}.join}"
   target = Autotest.const_get(style.map {|s| s.capitalize}.join)
 end
 target.run

lib/autotest.rb
@@ -253,7 +253,7 @@ class Autotest
       open("| #{cmd}", "r") do |f|
         until f.eof? do
           c = f.getc or break
-          print c
+          print (c.is_a?(Fixnum) ? c.chr : c)
           line << c
           if c == ?\n then
             self.results << if RUBY_VERSION >= "1.9" then
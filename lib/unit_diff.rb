673c3d46e92bffbba30b252369054bee3fda7867
Ryan Davis <ryand@zenspider.com>
2010-11-19 16:56:44 -0800

    + Cleaned up unit_diff matcher by using any? [git-p4: depot-paths = "//src/ZenTest/dev/": change = 5987]

--- lib/unit_diff.rb
+++ lib/unit_diff.rb
@@ -97,9 +97,7 @@ class UnitDiff
     footer = data.pop
 
     data.map do |result|
-      break if result.find do |result_line|
-        result_line =~ / expected( but was|, not)/
-      end
+      break if result.any? { |line| line =~ / expected( but was|, not)/ }
 
       header = result.find do |result_line|
         result_line =~ /^\(?\s*\d+\) (Failure|Error):/
